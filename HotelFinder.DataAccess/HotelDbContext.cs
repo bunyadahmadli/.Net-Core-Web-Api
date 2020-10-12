f618f4b25f146515dcdab183bb9f2ec42f49f587
Bunyad Ahmadli <34852029+bunyadahmadli@users.noreply.github.com>
2020-10-12 10:05:37 +0300

    first commit

---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelFinder.DataAccess
+{
+   public class HotelDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=DESKTOP-M28HTI4; Database=HotelDb;uis=sa;pwd=0056880b;");
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
